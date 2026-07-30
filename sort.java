2242345b1596fd9dc229cdf37cdb97716b668000
TANMAY TUKARAM PEDANEKAR <tanmaypedanekar.cse@gmail.com>
2026-07-30 22:57:26 +0530

    add sort

---
+++ sort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 9, 1, 3};
+
+        Arrays.sort(numbers);
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
