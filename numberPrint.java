5fbe080f6b7a1aa8823c6b42df1a8c9be98081d6
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-07-31 22:34:30 +0530

    Add number printing

---
+++ numberPrint.java
@@ -0,0 +1,14 @@
+public class numberPrint {
+    public static void main(String[] args) {
+        int num=1;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<=i;j++){
+
+                System.out.print(num+" ");
+              
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
