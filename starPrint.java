a58d053e369805bf3124c805c455ee7c05d9c2e2
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-01 22:05:54 +0530

    Add print star pattern

--- starPrint.java
+++ starPrint.java
@@ -3,8 +3,8 @@ public class starPrint {
     public static void main(String[] args) {
         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n-i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
