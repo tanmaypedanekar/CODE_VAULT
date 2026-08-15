e331446cd8dbc37c9cf335d801fc23a7332cd8d2
tanmaypedanekar <tanmaypedanekar.cse@gmail.com>
2026-08-15 19:51:16 +0530

    Add simple code for practice class

---
+++ classSecond.java
@@ -0,0 +1,23 @@
+ class classPractice {
+    int num;
+    String name;
+    double number;
+    boolean fact;
+
+}
+
+public class classSecond {
+    public static void main(String[] args) {
+        classPractice tan = new classPractice();
+        tan.num = 18;
+        tan.name = "tanmay";
+        tan.number = 70.30;
+        tan.fact = true;
+
+        System.out.println(tan.num);
+        System.out.println(tan.name);
+        System.out.println(tan.number);
+        System.out.println(tan.fact);
+    }
+
+}
